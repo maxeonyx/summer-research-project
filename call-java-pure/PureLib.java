67e285adc74b1ea2c9e0907bb86f06304f84d158
Maxwell Clarke <clarkemaxw@bangalore-polo-club.ecs.vuw.ac.nz>
2020-11-10 10:54:29 +1300

    Initial commit. Demos for calling purity-checked code from wyvern.

---
+++ call-java-pure/PureLib.java
@@ -0,0 +1,11 @@
+import org.checkerframework.dataflow.qual.*;
+
+public class PureLib {
+
+    public static final PureLib lib = new PureLib();
+
+    public @Pure String getMessageSound() {
+        return "Message from Purity-checked java.";
+    }
+
+}
